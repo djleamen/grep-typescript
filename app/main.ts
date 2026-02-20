7f10c999fab6b3675fc14ce25373c90ecb37ace9
DJ Leamen <dilara_leamen@icloud.com>
2026-02-20 09:12:22 -0500

    \d pattern detection

--- app/main.ts
+++ app/main.ts
@@ -9,7 +9,14 @@ const pattern = args[3];
 const inputLine: string = await Bun.stdin.text();
 
 function matchPattern(inputLine: string, pattern: string): boolean {
-  if (pattern.length === 1) {
+  if (pattern === "\\d") {
+    for (const char of inputLine) {
+      if (char >= '0' && char <= '9') {
+        return true;
+      }
+    }
+    return false;
+  } else if (pattern.length === 1) {
     return inputLine.includes(pattern);
   } else {
     throw new Error(`Unhandled pattern: ${pattern}`);
@@ -18,5 +25,11 @@ function matchPattern(inputLine: string, pattern: string): boolean {
 
 if (args[2] !== "-E") {
   console.log("Expected first argument to be '-E'");
+  process.exit(1);
+}
+
+if (matchPattern(inputLine, pattern)) {
+  process.exit(0);
+} else {
   process.exit(1);
 }
